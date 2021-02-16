8b5feb290d1831706a41bcf728b49bd54d5ce419
friedforfun <samfayhunt@gmail.com>
2021-02-16 12:06:00 +0000

    Working on yaml stuff

--- Experiment/Agents/Producer/ProducerAgent/main.py
+++ Experiment/Agents/Producer/ProducerAgent/main.py
@@ -12,6 +12,7 @@ from AgentBase.Utils.Logging import logger
 from ProducerAgent.distiller_interaction import Distiller, CompressionParams
 from ProducerAgent.filewatcher import FileCreationWatcher
 from ProducerAgent.redismanager import RedisConnectionManager
+from ProducerAgent.yamlEditor import YamlEditor
 
 
 def main():
@@ -35,48 +36,66 @@ def run(args):
 
         add_compress_classifier_to_path(distiller_path)
 
-        redis_conn = RedisConnectionManager(AGENT_ID, redis_host, port=redis_port, db=0, password=redis_password)
+        yaml_test = YamlEditor(yaml_path)
+        yaml_test.print()
 
-        watcher = FileCreationWatcher()
-        watcher.add_redis_to_event_handler(redis_conn)
 
-        watcher_thread = Thread(target=watcher.run, daemon=True)
-        watcher_thread.start()
+        yaml_test.yaml_object['pruners']['fc_pruner']['final_sparsity'] = 0.99
 
-        distiller_params = CompressionParams(model, data_path, yaml_path, epochs=2, lr=0.3, j=6, deterministic=True)
-        compressor = Distiller(distiller_params)
-        compressor.run()
+        print(yaml_test.yaml_object.get('pruners').get('fc_pruner'))
 
-        watcher.terminate()
-        watcher_thread.join()
-        logger.setLevel(5)
-        if watcher.path is None:
-            raise ValueError('No path for checkpoint!')
-        logger.debug('!! ------------------------------------------------------ !!')
-        logger.debug('Checkpoint path: {}'.format(watcher.path))
-        logger.debug('!! ------------------------------------------------------ !!')
+        modification_name = 'test'
+        modification_path = ['pruners', 'fc_pruner', 'final_sparsity']
 
-        watcher = FileCreationWatcher()
-        watcher.add_redis_to_event_handler(redis_conn)
-        watcher_thread = Thread(target=watcher.run, daemon=True)
-        watcher_thread.start()
+        yaml_test.add_modification_path(modification_name, modification_path)
 
-        distiller_onnx_params = CompressionParams(model, data_path, yaml_path, epochs=2, lr=0.3, j=6, deterministic=True, onnx='test_model.onnx', resume_from=watcher.path)
-        onnx_generator = Distiller(distiller_onnx_params)
-        onnx_generator.run()
 
-        watcher.terminate()
-        watcher_thread.join()
-        logger.setLevel(0)
-        redis_onnx = str(os.path.abspath(redis_conn.get_onnx().decode('utf-8')))
-        print('ONNX path FROM REDIS: {}'.format(redis_onnx))
-        logger.debug('ONNX path: {}'.format(watcher.onnx_path))
+        yaml_test.set_value_by_id('test', 0.5)
 
-        data_for_consumer = json.dumps({'Agent_ID': AGENT_ID, 'Model-UUID': str(compressor.onnx_id), 'ONNX': redis_onnx, 'Sender_IP': get_lan_ip(), 'User': 'sam'})
-        redis_conn.publish_model(data_for_consumer)
+        print(yaml_test.yaml_object.get('pruners').get('fc_pruner'))
 
-        redis_conn.listen_blocking(lambda msg: print(msg), lambda _: True)
-        print('Got response from Benchmarker')
+        # redis_conn = RedisConnectionManager(AGENT_ID, redis_host, port=redis_port, db=0, password=redis_password)
+
+        # watcher = FileCreationWatcher()
+        # watcher.add_redis_to_event_handler(redis_conn)
+
+        # watcher_thread = Thread(target=watcher.run, daemon=True)
+        # watcher_thread.start()
+
+        # distiller_params = CompressionParams(model, data_path, yaml_path, epochs=2, lr=0.3, j=6, deterministic=True)
+        # compressor = Distiller(distiller_params)
+        # compressor.run()
+
+        # watcher.terminate()
+        # watcher_thread.join()
+        # logger.setLevel(5)
+        # if watcher.path is None:
+        #     raise ValueError('No path for checkpoint!')
+        # logger.debug('!! ------------------------------------------------------ !!')
+        # logger.debug('Checkpoint path: {}'.format(watcher.path))
+        # logger.debug('!! ------------------------------------------------------ !!')
+
+        # watcher = FileCreationWatcher()
+        # watcher.add_redis_to_event_handler(redis_conn)
+        # watcher_thread = Thread(target=watcher.run, daemon=True)
+        # watcher_thread.start()
+
+        # distiller_onnx_params = CompressionParams(model, data_path, yaml_path, epochs=2, lr=0.3, j=6, deterministic=True, onnx='test_model.onnx', resume_from=watcher.path)
+        # onnx_generator = Distiller(distiller_onnx_params)
+        # onnx_generator.run()
+
+        # watcher.terminate()
+        # watcher_thread.join()
+        # logger.setLevel(0)
+        # redis_onnx = str(os.path.abspath(redis_conn.get_onnx().decode('utf-8')))
+        # print('ONNX path FROM REDIS: {}'.format(redis_onnx))
+        # logger.debug('ONNX path: {}'.format(watcher.onnx_path))
+
+        # data_for_consumer = json.dumps({'Agent_ID': AGENT_ID, 'Model-UUID': str(compressor.onnx_id), 'ONNX': redis_onnx, 'Sender_IP': get_lan_ip(), 'User': 'sam'})
+        # redis_conn.publish_model(data_for_consumer)
+
+        # redis_conn.listen_blocking(lambda msg: print(msg), lambda _: True)
+        # print('Got response from Benchmarker')
         #watcher = FileCreationWatcher()
         #watcher.run()
         #print('LOG DIR: {}'.format(logdir))
--- Experiment/Agents/Producer/ProducerAgent/yamlEditor.py
+++ Experiment/Agents/Producer/ProducerAgent/yamlEditor.py
@@ -1,5 +1,35 @@
+import yaml
+
+from AgentBase.Utils.Logging import logger
 # Read YAML file
 # Select params to modify
 # write copy with modified params
 
 # send message with location of modified yaml file
+
+class YamlEditor:
+    def __init__(self, yaml_path):
+        self.yaml_path = yaml_path
+        self.yaml_object = self.read_yaml()
+        self.modification_dict = {}
+
+    def read_yaml(self):
+        with open(self.yaml_path) as f:
+            return yaml.load(f)
+    
+    def print(self):
+        print(self.yaml_object)
+
+    def add_modification_path(self, id, list_path_to_rule):
+        self.modification_dict[id] = list_path_to_rule
+
+    def set_value_by_id(self, id, value):
+        path = self.modification_dict.get(id)
+        if path is not None:
+            yaml_seek = self.yaml_object[path[0]]
+            for p in path[1:-1]:
+                yaml_seek = yaml_seek[p]
+
+            yaml_seek[path[-1]] = value
+        else:
+            raise ValueError('Rule ID is not valid')
